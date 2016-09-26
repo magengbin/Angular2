a64f1c60a00b1f6e59eaac9fa0d2199c30cdda51
marson <marson@futunn.com>
2016-09-26 19:45:54 +0800

    update

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,46 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular App</h1>'
+    //template: '<h1>My First Angular App</h1>'
+    //template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>'
+    //template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2><div><label>id: </label>{{hero.id}}</div><div><label>name: </label>{{hero.name}}</div>'
+    /*template:`
+        <h1>{{title}}</h1>
+        <h2>{{hero.name}} details!</h2>
+        <div style="color:red"><label>id: </label>{{hero.id}}</div>
+        <div style="color:green">
+            <label>name: </label>
+            <input [(ngModel)]="hero.name" placeholder="name">
+        </div>
+        `
+    */
+    template:`
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+        <li *ngFor="let hero of heroes">
+            <span class="badge">{{hero.id}}</span> {{hero.name}}
+        </li>
+    </ul>
+    `
 })
-export class AppComponent { }
+
+export class AppComponent { 
+    title = 'Tour of Heroes';
+    heroes = HEROES;
+}
+const HEROES: Hero[] = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
+export class Hero {
+  id: number;
+  name: string;
+}
